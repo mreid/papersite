d16f89859d25872f538792d1b09f3f184298dd18
Mark Reid <mark@reid.name>
2013-02-15 16:34:54 +1100

    Clean up. Started adding supp and pdf files

--- site.hs
+++ site.hs
@@ -84,7 +84,7 @@ main = hakyllWith config $ do
 
   -- Supplementary files
   match "db/*/*/supplementary/*" $ do
-    route gsubRoute "db/" (const "")
+    route $ gsubRoute "db/" (const "")
     compile copyFileCompiler
 
   -- Templates
@@ -113,9 +113,9 @@ main = hakyllWith config $ do
 --------------------------------------------------------------------------------
 config :: Configuration
 config = defaultConfiguration
-  -- { deployCommand = "rsync --checksum -ave 'ssh -p 2222' \
-  --           \_site/* jaspervdj@jaspervdj.be:jaspervdj.be/tmp/hakyll4"
-  -- }
+  { deployCommand = "rsync --checksum -ave 'ssh -p 2222' \
+            \_site/* mreid@login.csail.mit.edu:"
+  }
 
 --------------------------------------------------------------------------------
 -- | Set a field of a page to a listing of pages
@@ -178,103 +178,3 @@ conferenceEntry paperID =
   in loadSnapshot confID "conference"
 
 
-{-
-runHakyll :: IO()
-runHakyll = hakyllWith config $ do
-  
-  -- Preprocessing: read in paper and author identifiers
-  bibdb <- preprocess $ parseBibFile <$> readFile bibFilePath
-  
-  let authorsPattern  = fromList $ map authorID $ keys $ authorToPapers bibdb
-  let papersPattern   = fromList $ map paperID $ keys $ paperToAuthors bibdb
-
-  -- Templates
-  match "templates/*" $ 
-    compile templateCompiler
-
-  -- Create a reference for every paper
-  forM_ (keys $ paperToAuthors bibdb) $ \paper -> do
-    let paperPattern = paperID paper
-    
-    create [ paperPattern ] $ compile $ makeItem paper
-    
-    create [ paperURI paper ] $ do
-      route (setExtension "html")
-      compile $ do
-        let authors         = (paperToAuthors bibdb) ! paper
-        
-        authorItems <- loadAll $ fromList $ map authorID authors
-        linkTpl     <- loadBody "templates/author-link.html"
-        authorLinks <- joinTemplateList linkTpl authorContext authorItems ", "
-        
-        let paperCtx = constField "authors" authorLinks `mappend` paperContext
-        
-        makeItem paper
-          >>= loadAndApplyTemplate "templates/paper.html" paperCtx
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
-          >>= relativizeUrls
-
-  -- Create a reference for every author
-  forM_ (keys $ authorToPapers bibdb) $ \author -> do
-    
-    create [ authorID author ] $ compile $ makeItem author
-
-    create [ authorURI author ] $ do
-      route (setExtension ".html")
-      compile $ do
-        let papers        = (authorToPapers bibdb) ! author
-        let papersPattern = fromList (map paperID papers)
-        
-        paperItems  <- loadAll papersPattern
-        linkTpl     <- loadBody "templates/paper-link.html"
-        paperLinks  <- applyTemplateList linkTpl paperContext paperItems
-        
-        let authorCtx =
-                constField "title" (name author)  `mappend`
-                constField "papers" paperLinks    `mappend`
-                authorContext
-        
-        makeItem author
-          >>= saveSnapshot "author"
-          >>= loadAndApplyTemplate "templates/author.html" authorCtx
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
-          >>= relativizeUrls
-
-  -- Author list
-  create ["authors.html"] $ do
-    route idRoute
-    compile $ do
-      
-      authors <- loadAll authorsPattern   
-      itemTpl <- loadBody "templates/author-item.html"
-      list    <- applyTemplateList itemTpl authorContext authors
-      
-      let authorsCtx =
-            constField "title" "All Authors"  `mappend`
-            constField "authors" list         `mappend`
-            defaultContext
-        
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/authors.html" authorsCtx
-        >>= loadAndApplyTemplate "templates/default.html" authorsCtx
-        >>= relativizeUrls
-
-  -- Paper list
-  create ["papers.html"] $ do
-    route idRoute
-    compile $ do
-      
-      papers  <- loadAll papersPattern
-      itemTpl <- loadBody "templates/paper-item.html"
-      list  <- applyTemplateList itemTpl paperContext papers
-      
-      let papersCtx =
-            constField "title" "All Papers" `mappend`
-            constField "papers" list        `mappend`
-            defaultContext
-        
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/papers.html" papersCtx
-        >>= loadAndApplyTemplate "templates/default.html" papersCtx
-        >>= relativizeUrls
--}
