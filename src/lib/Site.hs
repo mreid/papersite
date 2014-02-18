a5552692f32b8eb30da110e0b874f7ef4eaccb51
Mark Reid <mark@reid.name>
2014-02-18 23:55:25 +1100

    Deploy to newsite. Handle page numbers like 15.1

--- src/lib/Site.hs
+++ src/lib/Site.hs
@@ -102,14 +102,14 @@ config = defaultConfiguration
 	deployCommand = 
       "rsync --checksum -avz --no-group /tmp/jmlr/* " 
         ++ "mreid@login.csail.mit.edu:"
-        ++ "/afs/csail.mit.edu/group/jmlr/docroot/proceedings/papers/"
+        ++ "/afs/csail.mit.edu/group/jmlr/docroot/proceedings/newsite/"
   }
 
 -- FIXME: Make sure this can handle roman (e.g., "xvi") page numbers.
 pageSort :: [Item Entry] -> [Item Entry]
 pageSort = sortBy (\i1 i2 -> compare (page i1) (page i2))
   where
-	page = (read :: String -> Int) . fromJust . (getField "firstpage")
+	page = (read :: String -> Float) . fromJust . (getField "firstpage")
 
 -- Define a context for template fields using a given entry.
 -- This context will look for a matching field in the Entry's BibTeX fields
