081fe26ac621b488ae386ade4c178cc7ddcc9156
Mark Reid <mark@reid.name>
2014-03-25 14:02:39 +1100

    Remove Config.hs

--- src/lib/Config.hs
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------------------------------
-module Config 
-  ( baseURI )
-where
-
--- Base URI for the W&CP site
-baseURI = "http://jmlr.org/proceedings/papers/"
