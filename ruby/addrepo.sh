54d7835ddd93c648a10090e1ddbccc1b39de788e
Neil Lawrence <lawrennd@gmail.com>
2015-10-12 10:23:12 +0100

    Add some ruby code for hacking around with new site

--- ruby/addrepo.sh
+++ ruby/addrepo.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 git init
 git checkout -b gh-pages
 git add .
-git remote add origin "https://github.com/mlresearch/gpip2006"
+git remote add origin "https://github.com/mlresearch/$1"
 git remote -v
-git commit -a -m "Add gpip2006 pages"
+git commit -a -m "Add $1 pages"
 git push --set-upstream origin gh-pages
