68018ab99130bac090a9bc14208818c3affd9249
Mark Reid <mark@reid.name>
2017-05-31 21:04:07 -0700

    Use git@github origin for publickey access

--- ruby/addrepo.sh
+++ ruby/addrepo.sh
@@ -2,7 +2,7 @@
 git init
 git checkout -b gh-pages
 git add .
-git remote add origin "https://github.com/mlresearch/v$1"
+git remote add origin "git@github.com:mlresearch/v$1"
 git remote -v
 git commit -a -m "Add v$1 pages"
 git push --set-upstream origin gh-pages
