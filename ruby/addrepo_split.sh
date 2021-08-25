e33f8da7a83e496f3faabf4a5051f659e7f99603
Neil Lawrence <lawrennd@gmail.com>
2021-08-25 09:43:08 +0100

    Correct colon in add repo scripts.

--- ruby/addrepo_split.sh
+++ ruby/addrepo_split.sh
@@ -2,7 +2,7 @@
 git init
 git checkout -b gh-pages
 git add README.md _posts index.html *.bib Gemfile _config.yml
-git remote add origin git@github.com/mlresearch/$1.git
+git remote add origin git@github.com:mlresearch/$1.git
 git remote -v
 git commit -a -m "Add $1 pages"
 git push --set-upstream origin gh-pages
